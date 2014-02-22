164136a40f6fdeb44ae886f7135cd9fb4d9d18ef
Omari Norman <omari@smileystation.com>
2014-02-22 11:42:16 -0500

    WIP

--- lib/Test/Sunlight.hs
+++ lib/Test/Sunlight.hs
@@ -1,10 +1,15 @@
 module Test.Sunlight where
 
+import qualified Data.ByteString as BS
 import Control.Exception
+import Foreign.Safe
 import System.Directory
 import Distribution.Version
 import System.Exit
 import Distribution.Package
+import System.IO
+import System.Process
+import Control.Concurrent.MVar
 
 -- | Runs an IO action within the given directory, and then returns
 -- to the original directory.
@@ -20,6 +25,50 @@ inDirectory inside a = bracket get rel run
       return d
     rel = setCurrentDirectory
     run = const a
+
+-- | Creates a process.  Its stdin is closed.  Returns handles to
+-- stdout and stderr, and the ProcessHandle.
+makeProcess
+  :: FilePath
+  -- ^ Run this command
+  -> [String]
+  -- ^ Arguments
+  -> IO (Handle, Handle, ProcessHandle)
+makeProcess c as = do
+  (Just hin, Just out, Just err, h) <- createProcess cp
+  hClose hin
+  return (out, err, h)
+  where
+    cp = CreateProcess { cmdspec = RawCommand c as
+                       , cwd = Nothing
+                       , env = Nothing
+                       , std_in = CreatePipe
+                       , std_out = CreatePipe
+                       , std_err = CreatePipe
+                       , close_fds = True
+                       , create_group = False }
+
+bufsize :: Int
+bufsize = 512
+
+-- | Runs a command, sending standard output and standard error to
+-- the terminal, while also returning the standard output.
+tee
+  :: FilePath
+  -- ^ Run this command
+  -> [String]
+  -- ^ Arguments
+  -> IO (ExitCode, BS.ByteString, BS.ByteString)
+tee cmd as = bracket (makeProcess cmd as) rel go
+  where
+    rel (out, err, _) = hClose out >> hClose err
+
+    go (out, err, hdl) = do
+      mvOut <- newEmptyMVar
+      mvErr <- newEmptyMVar
+      undefined
+
+-- | Reads from standard 
 
 -- | Result from installing the package's dependencies.
 data InstallDepsResult = InstallDepsResult
